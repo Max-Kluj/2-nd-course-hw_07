dfd684ff9996c5a4af3c1c27da624373d041d3dd
Max_kluj <makc_kluj@mail.ru>
2026-09-24 22:56:12 +0300

    made game 3

--- game-mini__3.js
+++ game-mini__3.js
@@ -1,74 +1,31 @@
-function simpleArithmetic() {
+function flipTheText() {
 
-let numberOfAttempts = 0;
+let textUser;
 
-let numberOfCorrectAnswers = 0;
-
-let a, b, correctAnswer;
-
-const operator = ['+', '-', '*', '/'];     
 
 
 while (true){
 
-        const oper = operator[Math.floor(Math.random() * operator.length)];    
-        
-        if (oper === '/') {
-            correctAnswer = Math.floor(Math.random() * 10) + 1;
-            b = Math.floor(Math.random() * 10) + 1;   
-            a = b * correctAnswer; 
-        } else {
-            a = Math.floor(Math.random() * 10) + 1;
-            b = Math.floor(Math.random() * 10) + 1;
-        }
-        
-        
-        const expression = a + ' ' + oper + ' ' + b; 
-        
-        if (oper === '+') {
-            correctAnswer = a + b;
-        
-        } else if (oper === '-') {
-            correctAnswer = a - b;
-        
-        } else if (oper === '*') {
-            correctAnswer = a * b;
-        }
-        
-        const answerUser = prompt(`Введите ответ на выражение ${expression}`);
+    textUser = prompt('Введите текст:');
 
-        if (answerUser === null) {
+    if (textUser === null) {
             alert('Вы нажали Отмена');
             return;
         }
 
-        if (answerUser.trim() === '') {
-            alert('Вы ничего не ввели, введите число');
+    if (textUser.trim() === '') {
+            alert('Вы ничего не ввели, введите текст');
             continue;
         }
 
-        let numUser = Number(answerUser);
+    const flipText = textUser.split('').reverse().join('');
 
-        if (isNaN(numUser)) {
-            alert('Вы ввели не цифру, введите число');
-            continue;
-        }
+    let again;
 
-        let again;
+    again = confirm(`Вы ввели следующий текст: ${textUser}.\n
+        Ваш перевернутый текст: ${flipText}. \n Хотите попробовать еще?`);
 
-        numberOfAttempts++;
-
-        if (correctAnswer === numUser) {
-            numberOfCorrectAnswers++;
-            again = confirm('Вы ввели ответ верно. Попробуем еще?');
-        } else {
-            again = confirm('Вы ввели ответ не верно. Попробуем еще?');
-            
-        } 
-
-        if (!again) break;
-    }
-
-    alert(`Вчего задач: ${numberOfAttempts}, из них правильных ответов ${numberOfCorrectAnswers}`);
+    if (!again) break;
 }
 
+}
